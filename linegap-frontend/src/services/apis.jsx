b0cf99179e456326769ad6031bb3bf3ba3e975c0
Ameer Suhail <tmsyameersuhail@gmail.com>
2026-03-12 22:24:55 +0530

    changed the vercel link mistak in the backend

--- linegap-frontend/src/services/apis.jsx
+++ linegap-frontend/src/services/apis.jsx
@@ -2,7 +2,7 @@ const apiLocal = 'http://localhost:3000'
 const apiIP = 'http://192.168.37.237:3000'
 const liveVercal = 'https://linegap.onrender.com'
 
-const api = apiLocal
+const api = liveVercal
 
 export const serverAPI = api + '/api'
 export const serverAPISocket = api
